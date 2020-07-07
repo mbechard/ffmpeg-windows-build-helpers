4c7a3d729954bcdfd8f1577cf1fbb8e16498c774
Malcolm Bechard <malcolm.bechard@gmail.com>
2020-07-06 22:05:34 -0400

    dont build lameMP3 as shared, since we are linking it only with ffmpeg,
    it can be static

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1281,7 +1281,7 @@ build_lame() {
   do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn
   cd lame_svn
     sed -i.bak "1{/^\xef\xbb\xbf$/d}" libmp3lame/i386/nasm.h # Remove a UTF-8 BOM that breaks nasm if it's still there; should be fixed in trunk eventually https://sourceforge.net/p/lame/patches/81/
-    generic_configure "--enable-nasm --disable-decoder --disable-frontend --disable-static --enable-shared"
+    generic_configure "--enable-nasm --disable-decoder"
     do_make_and_make_install
   cd ..
 }
