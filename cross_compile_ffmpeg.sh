6adef00cf77bc6c03de8ff075b53795455aa9bee
Malcolm Bechard <malcolm.bechard@gmail.com>
2024-11-18 19:17:30 -0500

    move to newer version of SDL2, don't use the repo directly

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -834,9 +834,8 @@ build_iconv() {
 }
 
 build_sdl2() {
-  download_and_unpack_file https://www.libsdl.org/release/SDL2-2.0.12.tar.gz
-  do_git_checkout https://github.com/libsdl-org/SDL.git SDL_git SDL2
-  cd SDL_git
+  download_and_unpack_file https://www.libsdl.org/release/SDL2-2.30.9.tar.gz
+  cd SDL2-2.30.9
     #apply_patch file://$patch_dir/SDL2-2.0.12_lib-only.diff
     if [[ ! -f configure.bak ]]; then
       sed -i.bak "s/ -mwindows//" configure # Allow ffmpeg to output anything to console.
